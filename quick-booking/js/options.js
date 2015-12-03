847ba26e365f02e40d8ae0e9e1b38f7e8609d3bb
Daniyar Artykov <redscauser@gmail.com>
2015-12-04 01:12:02 +0600

    1. auto login done 2. auto search started

--- quick-booking/js/options.js
+++ quick-booking/js/options.js
@@ -1,15 +1,68 @@
 $(document).ready(function() {
+	$('#journey-date').datepicker({
+		dateFormat: 'dd.mm.yy'
+	});
 	initializeSavedDetails();
+
+	$('#from-station').autocomplete({
+		source: 'https://epay.railways.kz/ktz4/json4.jsp',
+		minLength: 2,
+		select: function( event, ui ) {
+			setFrom(ui.item.id);
+		},
+		change: function( event, ui ) {
+			if(ui.item == null) {
+				setFrom('');
+			} else {
+				setFrom(ui.item.id);
+			}
+		}
+	});
+	$('#to-station').autocomplete({
+		source: 'https://epay.railways.kz/ktz4/json4.jsp',
+		minLength: 2,
+		select: function( event, ui ) {
+			setTo(ui.item.id);
+		},
+		change: function( event, ui ) {
+			if(ui.item == null) {
+				setTo('');
+			} else {
+				setTo(ui.item.id);
+			}
+		}
+	});
+
 });
+
+$('#revert-direction-btn').click(function() {
+	var from = $('#from-station').val();
+	var to = $('#to-station').val();
+	$('#from-station').val(to);
+	$('#to-station').val(from);
+
+	from = $('#from-station-hidden').val();
+	to = $('#to-station-hidden').val();
+	$('#from-station-hidden').val(to);
+	$('#to-station-hidden').val(from);
+});
+
 $('#save-btn').click(function() {
 	var b = {};
 	var railways = {
 			'login': $('#login').val(),
-			'password': $('#password').val()
+			'password': $('#password').val(),
+			'fromHidden': $('#from-station-hidden').val(),
+			'toHidden': $('#to-station-hidden').val(),
+			'from': $('#from-station').val(),
+			'to': $('#to-station').val(),
+			'journeyDate': $('#journey-date').val()
 	}
-	
+
 	b['railways'] = railways;
-	console.debug('set login: %s; password: %s', railways.login, railways.password);
+	console.debug('set login: %s; password: %s; from: %s; to: %s; from-hidden: %s; to-hidden: %s; journey-date: %s', 
+			railways.login, railways.password, railways.from, railways.to,
+			railways.fromHidden, railways.toHidden, railways.journeyDate);
 	chrome.storage.local.set(b);
 });
 
@@ -21,5 +74,18 @@ function initializeLoginControls(a) {
 	if (a && (a = a['railways'])) {
 		$('#login').val(a.login);
 		$('#password').val(a.password);
+		$('#from-station').val(a.from);
+		$('#to-station').val(a.to);
+		$('#from-station-hidden').val(a.fromHidden);
+		$('#to-station-hidden').val(a.toHidden);
+		$('#journey-date').val(a.journeyDate);
 	}
+}
+
+function setFrom(id) {
+	$('#from-station-hidden').val(id);
+}
+
+function setTo(id) {
+	$('#to-station-hidden').val(id);
 }
--- quick-booking/js/railways.js
+++ quick-booking/js/railways.js
@@ -1,58 +1,67 @@
 function loginDo(step) {
-	var req;
-	var login, pwd;
+	var currentUser = $('.btn.btn-block.dropdown-toggle');
+	if(!currentUser.length) {
+		var req;
+		var login, pwd;
+		chrome.storage.local.get('railways', function(a) {
+			if(a) {
+				login = a['railways'].login;
+				pwd = a['railways'].password;
 
-	chrome.storage.local.get('railways', function(a){
-		if(a) {
-			login = a['railways'].login;
-			pwd = a['railways'].password;
-
-			if (window.XMLHttpRequest) {
-				req = new XMLHttpRequest();
-			} else if(window.ActiveXObject) {
-				try {
-					req = new ActiveXObject('Msxml2.XMLHTTP');
-				} catch (e){}
-				try {
-					req = new ActiveXObject('Microsoft.XMLHTTP');
-				} catch (e){}
-			}
-
-			if (req) {
-				req.onreadystatechange = function(){
-					document.getElementById("sbmLogin").disabled="false";
-					document.getElementById("plsWait").innerHTML="";
-					if (req.readyState == 4 && req.status == 200){
-						var resp = req.responseText.trim();
-						if(resp=="LOGIN_OK"){
-							if(document.getElementById("LichCab")!=undefined && step==2){
-								document.location.href="proc?pa=clients"
-							}else{
-								updateUserDiv();
+				if (window.XMLHttpRequest) {
+					req = new XMLHttpRequest();
+				} else if(window.ActiveXObject) {
+					try {
+						req = new ActiveXObject('Msxml2.XMLHTTP');
+					} catch (e){}
+					try {
+						req = new ActiveXObject('Microsoft.XMLHTTP');
+					} catch (e){}
+				}
+				if (req) {
+					req.onreadystatechange = function(){
+						document.getElementById('sbmLogin').disabled='false';
+						document.getElementById('plsWait').innerHTML='';
+						if (req.readyState == 4 && req.status == 200){
+							var resp = req.responseText.trim();
+							if(resp == 'LOGIN_OK') {
+								if(document.getElementById('LichCab')!=undefined && step==2){
+									document.location.href='proc?pa=clients'
+								} else {
+									updateUserDiv();
+								}
+							} else {
+								var obj = document.getElementById('loginErrDiv');
+								obj.innerHTML='<font color="red"><b>'+resp+'</b></font>';
 							}
-							$('#modal-auth').modal('hide');
-						}else{
-							var obj = document.getElementById('loginErrDiv');
-							obj.innerHTML="<font color='red'><b>"+resp+"</b></font>";
 						}
-					}
-				};
-				var reqStr = "pa=clients&sa=LOGIN_EVENT&STEP="+step
-				+ "&CLIENT_LOGIN="+login
-				+ "&CLIENT_PWD="+pwd;
+					};
+					var reqStr = 'pa=clients&sa=LOGIN_EVENT&STEP='+step
+					+ '&CLIENT_LOGIN='+login
+					+ '&CLIENT_PWD='+pwd;
 
-				alert(reqStr);
-				
-				req.open("POST", '/ktz4/proc', true);
-				req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-				req.send(reqStr);
-				document.getElementById("sbmLogin").disabled="true";
-				document.getElementById("plsWait").innerHTML="подождите...";
-			} else {
-				alert("Браузер не поддерживает AJAX.");
+					req.open('POST', '/ktz4/proc', true);
+					req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+					req.send(reqStr);
+					document.getElementById('sbmLogin').disabled='true';
+					document.getElementById('plsWait').innerHTML='подождите...';
+				} else {
+					alert('Браузер не поддерживает AJAX.');
+				}
 			}
-		}
-	});
+		});
+	} else {
+		chrome.storage.local.get('railways', function(a) {
+			if(a && document.getElementById('FROM_STATION')) {
+				document.getElementById('FROM_STATION').value = a['railways'].from;
+				document.forms.form1.FROM_STATION.value = a['railways'].fromHidden;
+				document.getElementById('TO_STATION').value = a['railways'].to;
+				document.forms.form1.TO_STATION.value = a['railways'].toHidden;
+				document.forms.form1.DATE.value = a['railways'].journeyDate;
+				document.getElementById('jsCalendarInp').value = a['railways'].journeyDate;
+			}
+		});
+	}
 }
 
 function updateUserDiv(){
@@ -67,20 +76,18 @@ function updateUserDiv(){
 			req = new ActiveXObject('Microsoft.XMLHTTP');
 		} catch (e){}
 	}
-
 	if (req) {
 		req.onreadystatechange = function(){
 			if (req.readyState == 4 && req.status == 200){
-				document.getElementById("userDiv").innerHTML=req.responseText.trim();
-				loadFormLoginEvents();
+				document.getElementById('userDiv').innerHTML=req.responseText.trim();
 			}
 		};
-		req.open("POST", '/ktz4/include5/user_menu.jsp', true);
-		req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		req.send("");
-	}else{
-		alert("Браузер не поддерживает AJAX.");
+		req.open('POST', '/ktz4/include5/user_menu.jsp', true);
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		req.send('');
+	} else {
+		alert('Браузер не поддерживает AJAX.');
 	}
 }
 
-loginDo(0);
+loginDo(1);
