efb5a549cd46aebc12f305e777752847dab77d28
Daniyar Artykov <redscauser@gmail.com>
2016-01-11 00:13:53 +0600

    1. added new projects, working on intercom-gmail

---
+++ intercom-gmail/js/options.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	initializeSavedDetails();
+});
+
+$('#save-btn').click(function() {
+	var b = {};
+	var userData = {
+			'appId': $('#app-id').val(),
+			'apiKey': $('#api-key').val()
+	}
+
+	b['userData'] = userData;
+	chrome.storage.local.set(b);
+});
+
+function initializeSavedDetails() {
+	chrome.storage.local.get('userData', initializeUserDataControls);
+}
+
+function initializeUserDataControls(a) {
+	if (a && (a = a['userData'])) {
+		$('#app-id').val(a.appId);
+		$('#api-key').val(a.apiKey);
+	}
+}
