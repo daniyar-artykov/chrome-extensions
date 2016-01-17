1c6f7a02a79f93cb9dd242a352aa3431461df5a7
Daniyar Artykov <redscauser@gmail.com>
2016-01-18 00:44:51 +0600

    last changes

--- intercom-gmail/js/intercom-gmail.js
+++ intercom-gmail/js/intercom-gmail.js
@@ -22,7 +22,44 @@ function initializeUserDataControls(a) {
 					+ btoa(a.appId + ':' + a.apiKey)
 			},
 			success : function(result) {
+				$('#name').html(result.name);
+				$('#email').prop('href','mailito:'+result.email).text(result.email);
+				$('#city').html(result.location_data.city_name);
+				$('#country').html(result.location_data.country_name);
+				$('#session-count').html(result.session_count);
+				$('#tag-name').html(result.tags.tags[0].name);
+				if(result.companies.companies[0] != null 
+						&& result.companies.companies[0].company_id != null) {
+					$('#company').html(result.companies.companies[0].name);
 
+					// getting info about company
+					$.ajax({
+						url : 'https://api.intercom.io/companies',
+						type : 'GET',
+						data : {
+							id : result.companies.companies[0].company_id
+						},
+						dataType : 'json',
+						headers : {
+							Accept: 'application/json',
+							'Authorization' : 'Basic '
+								+ btoa(a.appId + ':' + a.apiKey)
+						},
+						success : function(result1) {
+							// $('#teammates').html(result1.user_count);
+							$.each(result1.companies, function(index, element) {
+								if(element.company_id == result.companies.companies[0].company_id) {
+									$('#teammates').html(element.user_count);
+								}
+							});
+						},
+						error : function(xhr, ajaxOptions,
+								thrownError) {
+							console.log(xhr.status);
+							console.log(thrownError);
+						}
+					});
+				}
 			},
 			error : function(xhr, ajaxOptions,
 					thrownError) {
