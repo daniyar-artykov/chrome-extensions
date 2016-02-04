bac3f88c25dd8e2da9171496e77cabfd738d9634
Daniyar Artykov <redscauser@gmail.com>
2016-02-05 00:23:42 +0600

    last changes

--- hidester-proxy/js/hidester.js
+++ hidester-proxy/js/hidester.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-	$('#hidester-form').submit();
+	var server = getParameterByName('server');
+	var url = getParameterByName('url');
+	
+	console.log(server + ' | ' + url);
+	if(server == 'proxy-eu') {
+		$('#hidester-form').prop('action', 'https://eu.hidester.com/proxy/includes/process.php?action=update');
+	} else {
+		$('#hidester-form').prop('action', 'https://us.hidester.com/proxy/includes/process.php?action=update')
+	}
+
+	$('#u').prop('value', url);
+
+	if($('#hidester-form').prop('action') && $('#u').prop('value')) {
+		$('#hidester-form').submit();
+	} else {
+		setTimeout(function () {
+			$('#hidester-form').submit();
+		}, 300);
+	}
 });
+
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
--- hidester-proxy/js/popup.js
+++ hidester-proxy/js/popup.js
@@ -1,10 +1,11 @@
 var proxies = ['us', 'eu'];
 
 document.addEventListener("DOMContentLoaded", function() {
-	
 	// add event listener to all checkboxes
 	for (var i = 0; i < proxies.length; ++i) {
 		document.getElementById('proxy-' + proxies[i]).addEventListener('click', function(e) {
+			var hidester_url = chrome.extension.getURL('/forms/hidester.html');
+
 			if(document.getElementById(e.target.id).checked) {
 				// change label color to active
 				document.getElementById(e.target.id + '-label').className += " active";
@@ -20,6 +21,23 @@ document.addEventListener("DOMContentLoaded", function() {
 							document.getElementById(check.id + '-label').className.replace(/(?:^|\s)active(?!\S)/g, '');
 					}
 				}
+				chrome.tabs.getSelected(null, function(tab) {
+					hidester_url = hidester_url + '?server=' + e.target.id + '&url=' + tab.url;
+					console.log(hidester_url);
+					chrome.tabs.query({
+						url: hidester_url
+					}, function(tabs) {
+						var props, _ref;
+						chrome.tabs.getSelected(null, function(tab) {
+							props = {
+									active: true
+							};
+							props.url = hidester_url;
+							chrome.tabs.update(tab.id, props);
+							window.close();
+						});
+					});
+				});				
 			} else {
 				// change label color to inactive
 				document.getElementById(e.target.id + '-label').className =
@@ -27,22 +45,5 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 		});
 	}
-	//var hidester_url = chrome.extension.getURL('/forms/hidester.html');
-//	document.getElementById('div-united-states').onclick = function() {
-//	//hidester_url = hidester_url.concat('&');
-//	chrome.tabs.query({
-//	url: hidester_url
-//	}, function(tabs) {
-//	var props, _ref;
-//	chrome.tabs.getSelected(null, function(tab) {
-//	props = {
-//	active: true
-//	};
-//	props.url = hidester_url;
-//	chrome.tabs.update(tab.id, props);
-//	window.close();
-//	});
-//	});
-//	}
 });
 
